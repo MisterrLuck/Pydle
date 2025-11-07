0b05fe5375d5f71c70cb4a476935acf5892a7a84
MisterrLuck <81490409+MisterrLuck@users.noreply.github.com>
2025-11-07 09:33:05 -0500

    Update main.py
    fixed formatting

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 from pydle import Pydle
 
-
-game = Pydle()
-game.reset()
-# game.answer = list("karek")
-# print(game.answer)
-# game.print_answer()
-while game.guesses < game.max_guesses:
-    guess = input("> ")
-    result = game.make_guess(guess)
-    print("= "+result)
-    if result == "GGGGG":
-        print("Congrats")
-        break
-if result != "GGGGG":
-    print("Oh well")
+if __name__ == "__main__":
+    game = Pydle()
+    game.reset()
+    while game.guesses < game.max_guesses:
+        guess = input(f"{game.max_guesses-game.guesses}> ")
+        result = game.make_guess(guess)
+        print("== "+result)
+        if result == "GGGGG":
+            print("Congrats")
+            break
+    if result != "GGGGG":
+        print("Oh well")
+        print("\n< ", end="")
+        game.print_answer()
