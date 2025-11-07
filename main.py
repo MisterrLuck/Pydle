4f104aa0c4571b28ac5031d331f018ad1170c3b1
Joshua <joshuacarpenter08@gmail.com>
2025-11-06 20:24:42 -0500

    Fixed dups for the yellows, and added simple interface

--- main.py
+++ main.py
@@ -3,7 +3,15 @@ from pydle import Pydle
 
 game = Pydle()
 game.reset()
-game.answer = "karek"
-print(game.answer)
-guess = input("> ")
-print("result: "+game.make_guess(guess))
+# game.answer = list("karek")
+# print(game.answer)
+# game.print_answer()
+while game.guesses < game.max_guesses:
+    guess = input("> ")
+    result = game.make_guess(guess)
+    print("= "+result)
+    if result == "GGGGG":
+        print("Congrats")
+        break
+if result != "GGGGG":
+    print("Oh well")
--- pydle.py
+++ pydle.py
@@ -3,63 +3,90 @@ import random
 class Pydle:
     def __init__(self):
         self.possible_words = ['games', 'fried', 'short', 'orate', 'audio']
-        self.answer = None
+        self.answer = []
         self.max_guesses = 6
         self.guesses = 0
     
     def reset(self):
         self.random_word()
         self.guesses = 0
+    
+    def print_answer(self):
+        print(''.join(self.answer))
 
     def random_word(self):
-        self.answer = random.choice(self.possible_words)
+        self.answer = list(random.choice(self.possible_words))
 
     def make_guess(self, word_guess: str) -> str:
         if len(word_guess) != 5:
             raise Exception("Only 5 letter guesses")
-        if word_guess == self.answer:
+
+        list_guess = list(word_guess)
+        answer_copy = self.answer.copy()
+        if list_guess == self.answer:
             return "GGGGG"
 
-        dups = ""
-        for i in range(5):
-            count = word_guess.count(word_guess[i])
-            if count > 0 :
-                dups += word_guess[i]
+        # dups = ""
+        # for i in range(5):
+        #     count = word_guess.count(word_guess[i])
+        #     if count > 0 :
+        #         dups += word_guess[i]
 
+        # print(list_guess)
+        # print(self.answer)
 
-        greens = ""
+        greens = []
         # Find the Greens
         for i in range(5):
-            if word_guess[i] == self.answer[i]:
-                greens += "G"
+            if list_guess[i] == answer_copy[i]:
+                greens.append("G")
+                list_guess[i] = "*"
+                answer_copy[i] = "*"
             else:
-                greens += "-"
+                greens.append("-")
+
+        # print(f"Guess:  {list_guess}")
+        # print(f"Answer: {answer_copy}")
         
-        yellows = ""
+        
+        yellows = []
         # Find the Yellows
-        for i in range(5):
-            if word_guess[i] in self.answer:
-                if word_guess.count(word_guess[i]) == self.answer.count(word_guess[i]):
-                    yellows += "Y"
-                else:
-                    yellows += "P"
+        for char in list_guess:
+            if char in answer_copy:
+                # print(f"bef answer: {answer_copy}")
+                # print(f"bef guess: {list_guess}")
+                # print(f"bef yellow: {yellows}")
+                # print(f"char: {char}")
+                answer_copy.remove(char)
+                # list_guess.remove(char)
+                # if list_guess.count(list_guess[i]) == answer_copy.count(list_guess[i]):
+                yellows.append("Y")
+                # print(f"aft answer: {answer_copy}")
+                # print(f"aft guess: {list_guess}")
+                # print(f"aft yellow: {yellows}")
+                # print()
+                # else:
+                #     yellows.append("D")
             else:
-                yellows += "-"
+                # print(f"aft yellow: {yellows}")
+                yellows.append("-")
+                # print(f"aft yellow: {yellows}")
+                # print()
                     
         self.guesses += 1
-        return self.union(greens, yellows)
+        return ''.join(self.union(greens, yellows))
     
-    def union(self, first: str, second: str):
-        print("first:  "+first)
-        print("second: "+second)
+    def union(self, first: list[str], second: list[str]) -> list[str]:
+        # print("first:  "+ ''.join(first))
+        # print("second: "+''.join(second))
         if len(first) != len(second) or len(first) != 5:
             raise Exception("Only 5 letter guesses")
-        unioned = ""
+        unioned = []
         for i in range(5):
             if first[i] == "G" or second[i] == "G":
-                unioned += "G"
+                unioned.append("G")
             elif first[i] == "Y" or second[i] == "Y":
-                unioned += "Y"
+                unioned.append("Y")
             else:
-                unioned += "-"
+                unioned.append("-")
         return unioned
